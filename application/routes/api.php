e86de49fda8cd63b7907c1994f28b5d1044007b0
Adam Alexander Campbell <adamalexandercampbell@gmail.com>
2024-06-03 16:56:08 +0100

    Added basic health check

--- application/routes/api.php
+++ application/routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\HealthCheckController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+/*
+ |-----------------------------------------------------------------------------
+ | HEALTH CHECKS
+ |-----------------------------------------------------------------------------
+ | Routes dedicated to health checks and debugging
+ */
+Route::group([
+    'prefix' => 'health-check'
+], function () {
+    Route::get('', [HealthCheckController::class, 'check']);
+});
